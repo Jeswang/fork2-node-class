4c17b8956c4579ddf7b20b1a2174c18ec1642e69
jeswang <wangyi724@gmail.com>
2014-05-04 22:11:16 +0800

    Implement Class Inheritance

--- Makefile
+++ Makefile
@@ -3,4 +3,10 @@ warm4:
 	mocha verify -R spec -g 'Implement Class Constructor'
 	mocha verify -R spec -g 'Implement Instance Methods'
 
+warm5:
+	mocha verify -R spec -g "Implement Class __super__"
+	mocha verify -R spec -g 'Implement Methods Inheritance'
+	mocha verify -R spec -g "Implement Super call"
+	mocha verify -R spec -g "Implement Super's Super"
+
 .PHONY: warm4
--- index.js
+++ index.js
@@ -1,20 +1,68 @@
 #!/usr/bin/env node
 
-function Class(input){
-  
+function Class(input, superClass){
+  var obj;  
   if(input.initialize){
-    obj = input.initialize;
+    obj = function(){
+      input.initialize.apply(this, [].slice.call(arguments,0));
+    }
   }
   else{
     obj = function(){}
   }
-  
-  for(key in input){
+
+  function ctor() {
+    this.constructor = obj;
+  }
+
+  obj.prototype = new ctor();
+  //obj.prototype.constructor = obj;
+
+
+  if(superClass){
+    for(var key in superClass.prototype){
+      //console.log(key);
+      if(key!='constructor'){
+        obj.prototype[key] = superClass.prototype[key];
+      }
+    }
+  }
+
+  if(superClass){
+    obj.__super__ = superClass;
+    ctor.prototype = new superClass();
+  }
+  else{
+    obj.__super__ = Object;
+    ctor.prototype = new Object();
+  }
+
+  ctor.prototype.constructor = ctor;
+ 
+  for(var key in input){
     if(key != "initialize"){
       obj.prototype[key] = input[key]; 
     }
   }
   
+  var current_class = obj;
+  var count = 0;
+
+  obj.prototype.super = function(name){
+    parent = current_class.__super__;
+    if(name){
+      current_class = current_class.__super__;
+      count = count + 1;
+      result = parent.prototype[name].apply(this, [].slice.call(arguments,1));
+      count = count - 1;
+      if(count==0){
+        current_class = obj;
+      }
+      //console.log(result);
+      return result;
+    }
+  };
+
   return obj;
 }
 
